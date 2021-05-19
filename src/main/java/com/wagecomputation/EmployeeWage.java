db17d6abaf6984f01f2f94764525042ac5c710bb
unknown <naveentruly86@gmail.com>
2021-05-19 16:58:33 +0530

    Added: Employee Part time Wage

--- src/main/java/com/wagecomputation/EmployeeWage.java
+++ src/main/java/com/wagecomputation/EmployeeWage.java
@@ -4,14 +4,20 @@ public class EmployeeWage {
     public static void main(String[] args)
     {
         int PRESENT = 1;
+        int PART_TIME = 2;
         int wage_per_hour = 20;
+        int part_day_hour = 4;
         int full_day_hour = 8;
 
-        double check = (Math.floor(Math.random() * 10) % 2);
+        double check = (Math.floor(Math.random() * 10) % 3);
         if ((int) check == PRESENT) {
             System.out.println("Employee is present");
             double wage = wage_per_hour * full_day_hour;
             System.out.println("Daily Wage : " + wage);
+        } else if((int) check == PART_TIME) {
+            System.out.println("Employee is half day present");
+            double wages = wage_per_hour * part_day_hour;
+            System.out.println("Daily Wage : " + wages);
         } else {
             System.out.println("Employee is absent.so no wage");
         }
