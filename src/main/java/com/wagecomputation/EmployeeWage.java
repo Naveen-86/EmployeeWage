2bee61e764660e7f1735d71abac0e0d68e1a45fb
unknown <naveentruly86@gmail.com>
2021-05-19 17:18:53 +0530

    Added: Calculating Monthly Wage

--- src/main/java/com/wagecomputation/EmployeeWage.java
+++ src/main/java/com/wagecomputation/EmployeeWage.java
@@ -6,6 +6,7 @@ public class EmployeeWage {
         int wage_per_hour = 20;
         int part_day_hour = 4;
         int full_day_hour = 8;
+        int total_working_days = 20;
 
         double check = (Math.floor(Math.random() * 10) % 3);
         switch ((int) check) {
@@ -14,13 +15,19 @@ public class EmployeeWage {
                        System.out.println("Employee is present");
                        double wage = wage_per_hour * full_day_hour;
                        System.out.println("Daily Wage : " + wage);
+                       double salary = wage * total_working_days;
+                       System.out.println("Monthly Wage : " + salary);
                        break;
             case 2:
                        System.out.println("Employee is half day present");
                        double wages = wage_per_hour * part_day_hour;
                        System.out.println("Daily Wage : " + wages);
+                       double salaries = wages * total_working_days;
+                       System.out.println("Monthly Wage : " + salaries);
+                       break;
             case 0:
                        System.out.println("Employee is absent.so no wage");
+                       break;
         }
     }
 }
