39bff16ebd862473e511492f26307665cf4afe19
unknown <naveentruly86@gmail.com>
2021-05-19 17:08:52 +0530

    Solving using Switch case

--- src/main/java/com/wagecomputation/EmployeeWage.java
+++ src/main/java/com/wagecomputation/EmployeeWage.java
@@ -3,23 +3,24 @@ package com.wagecomputation;
 public class EmployeeWage {
     public static void main(String[] args)
     {
-        int PRESENT = 1;
-        int PART_TIME = 2;
         int wage_per_hour = 20;
         int part_day_hour = 4;
         int full_day_hour = 8;
 
         double check = (Math.floor(Math.random() * 10) % 3);
-        if ((int) check == PRESENT) {
-            System.out.println("Employee is present");
-            double wage = wage_per_hour * full_day_hour;
-            System.out.println("Daily Wage : " + wage);
-        } else if((int) check == PART_TIME) {
-            System.out.println("Employee is half day present");
-            double wages = wage_per_hour * part_day_hour;
-            System.out.println("Daily Wage : " + wages);
-        } else {
-            System.out.println("Employee is absent.so no wage");
+        switch ((int) check) {
+
+            case 1:
+                       System.out.println("Employee is present");
+                       double wage = wage_per_hour * full_day_hour;
+                       System.out.println("Daily Wage : " + wage);
+                       break;
+            case 2:
+                       System.out.println("Employee is half day present");
+                       double wages = wage_per_hour * part_day_hour;
+                       System.out.println("Daily Wage : " + wages);
+            case 0:
+                       System.out.println("Employee is absent.so no wage");
         }
     }
 }
