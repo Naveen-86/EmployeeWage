991ee79e14ebec44c50e9080939d6f1e58b29238
unknown <naveentruly86@gmail.com>
2021-05-19 17:28:21 +0530

    Added: Calculating Wage till a Condition

--- src/main/java/com/wagecomputation/EmployeeWage.java
+++ src/main/java/com/wagecomputation/EmployeeWage.java
@@ -4,30 +4,35 @@ public class EmployeeWage {
     public static void main(String[] args)
     {
         int wage_per_hour = 20;
-        int part_day_hour = 4;
-        int full_day_hour = 8;
+        int part_day_hour = 3;
+        int full_day_hour = 5;
         int total_working_days = 20;
+        while( total_working_days <= 20 ) {
 
-        double check = (Math.floor(Math.random() * 10) % 3);
-        switch ((int) check) {
+            double check = (Math.floor(Math.random() * 10) % 3);
+            switch ((int) check) {
 
-            case 1:
-                       System.out.println("Employee is present");
-                       double wage = wage_per_hour * full_day_hour;
-                       System.out.println("Daily Wage : " + wage);
-                       double salary = wage * total_working_days;
-                       System.out.println("Monthly Wage : " + salary);
-                       break;
-            case 2:
-                       System.out.println("Employee is half day present");
-                       double wages = wage_per_hour * part_day_hour;
-                       System.out.println("Daily Wage : " + wages);
-                       double salaries = wages * total_working_days;
-                       System.out.println("Monthly Wage : " + salaries);
-                       break;
-            case 0:
-                       System.out.println("Employee is absent.so no wage");
-                       break;
+                case 1:
+                    System.out.println("Employee is present");
+                    double wage = wage_per_hour * full_day_hour;
+                    System.out.println("Daily Wage : " + wage);
+                    double salary = wage * total_working_days;
+                    total_working_days++;
+                    System.out.println("Monthly Wage : " + salary);
+                    break;
+                case 2:
+                    System.out.println("Employee is half day present");
+                    double wages = wage_per_hour * part_day_hour;
+                    System.out.println("Daily Wage : " + wages);
+                    double salaries = wages * total_working_days;
+                    total_working_days++;
+                    System.out.println("Monthly Wage : " + salaries);
+                    break;
+                case 0:
+                    System.out.println("Employee is absent.so no wage");
+                    total_working_days++;
+                    break;
+            }
         }
     }
 }
