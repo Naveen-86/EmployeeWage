1f09173e70fb2531bde80024080706cd3394ce59
unknown <naveentruly86@gmail.com>
2021-05-19 16:23:45 +0530

    Employee Attendance Checking

--- src/main/java/com/wagecomputation/EmployeeWage.java
+++ src/main/java/com/wagecomputation/EmployeeWage.java
@@ -3,6 +3,12 @@ package com.wagecomputation;
 public class EmployeeWage {
     public static void main(String[] args)
     {
-        System.out.println("Welcome to Employee Wage Computation Program");
+        int PRESENT = 1;
+        double check = (Math.floor(Math.random() * 10) % 2);
+        if ((int) check == PRESENT) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+        }
     }
 }
