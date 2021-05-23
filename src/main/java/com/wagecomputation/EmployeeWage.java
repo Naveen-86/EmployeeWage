0a0165bcf2b1c5962df18da54c2e433989d4cfc7
Naveen Madhaboina <79349929+Naveen-86@users.noreply.github.com>
2021-05-23 11:09:19 +0530

    Update EmployeeWage.java

--- src/main/java/com/wagecomputation/EmployeeWage.java
+++ src/main/java/com/wagecomputation/EmployeeWage.java
@@ -4,13 +4,13 @@ public class EmployeeWage {
     public static void main(String[] args)
     {
         int PRESENT = 1;
-        int wage_per_hour = 20;
-        int full_day_hour = 8;
+        int WAGE_PER_HOUR = 20;
+        int FULL_DAY_HOUR = 8;
 
         double check = (Math.floor(Math.random() * 10) % 2);
         if ((int) check == PRESENT) {
             System.out.println("Employee is present");
-            double wage = wage_per_hour * full_day_hour;
+            double wage = WAGE_PER_HOUR * FULL_DAY_HOUR;
             System.out.println("Daily Wage : " + wage);
         } else {
             System.out.println("Employee is absent.so no wage");
