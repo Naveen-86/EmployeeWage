1636a5fe7c0f2237c618e035ef6af10b7f7c6f21
Naveen <naveentruly86@gmail.com>
2021-05-23 20:36:04 +0530

    Added: Multiple Companies

--- src/main/java/com/wagecomputation/EmployeeWage.java
+++ src/main/java/com/wagecomputation/EmployeeWage.java
@@ -2,40 +2,49 @@ package com.wagecomputation;
 import java.util.*;
 
 public class EmployeeWage {
+        public static final int FULL_DAY_HOUR = 8;
+        public static final int HALF_DAY_HOUR = 4;
 
-    public static final int wagePerHour = 20;
-    public static final int partTimeHour = 4;
-    public static final int fullDayHour = 8;
-    public static final int totalWorkingDays = 20;
+        public static int calculateWage(String name,double wage,int workingDays,int workingHours)
+        {
 
-    public static int computeWage() {
+            //variables
+            int empWorkingDays = 0;
+            int empWorkingHrs = 0;
+            while( empWorkingDays < workingDays && empWorkingHrs < workingHours ) {
 
-        int workingHours = 0;
-        int workingDays = 0;
+                double check = (Math.floor(Math.random() * 10) % 3);
+                switch ((int) check) {
 
-        while ((workingDays < 20) && (workingHours < 100)){
-            Random n = new Random();
-            int ran = n.nextInt(3);
-            workingDays++;
-
-            switch(ran){
-                case 2:
-                    workingHours += fullDayHour;
-                    break;
-                case 1:
-                    workingHours += partTimeHour;
-                    break;
-                case 0:
-                    break;
-                default:
+                    case 1:
+                        empWorkingHrs += FULL_DAY_HOUR ;
+                        System.out.println("Full Time");
+                        break;
+                    case 2:
+                        empWorkingHrs += HALF_DAY_HOUR ;
+                        System.out.println("Part Time");
+                        break;
+                    case 0:
+                        empWorkingHrs = 0;
+                        System.out.println("absent");
+                        break;
+                }
+                empWorkingDays++;
             }
+
+            int totalEmpWage = empWorkingHrs * workingHours;
+            System.out.println(" Total Wage for Company : " + name + " : " + totalEmpWage);
+            return totalEmpWage;
         }
-        int totalEmpWage = workingHours * wagePerHour;
-        System.out.println("Total employee wage: " + totalEmpWage);
-        return totalEmpWage;
+
+        public static void main(String[] args) {
+
+            calculateWage("Dell",36000,22,176);
+            calculateWage("Lenovo",40000,23,184);
+            calculateWage("Infosys",42000,24,192);
+            calculateWage("Amazon",44000,25,200);
+            calculateWage("Wipro",46000,26,208);
+        }
     }
-    public static void main(String[] args) {
-        System.out.println("This is employee wage computation");
-        computeWage();
-    }
-}
+
+
