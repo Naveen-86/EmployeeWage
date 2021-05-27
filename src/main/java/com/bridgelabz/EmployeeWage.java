ab6c23395aeb8ecb3e421d751a910cca0544e975
Naveen <naveentruly86@gmail.com>
2021-05-27 21:19:04 +0530

    Added: Total Wage For Company

---
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,91 @@
+package com.bridgelabz;
+
+class WageCalculation
+{
+    //CONSTANTS
+    public static final int IS_PRESENT = 1;
+    public static final int IS_FULL_TIME = 1;
+
+    //VARIABLES
+    private final String company;
+    private final int wage_per_hr;
+    private final int working_days_per_month;
+    private final int max_working_hrs;
+
+    public WageCalculation(String company, int wage_per_hr, int working_days_per_month,int max_working_hrs)
+    {
+        this.company = company;
+        this.wage_per_hr = wage_per_hr;
+        this.working_days_per_month = working_days_per_month;
+        this.max_working_hrs = max_working_hrs;
+    }
+
+    //WAGE CALCULATOR
+    public int wageCalculator()
+    {
+        //VARIABLES
+        int checkPresence;
+        int checkEmpType;
+        //int empDailyWage = 0; //Used if uncomment the commented code-lines
+        int dayOfMonth = 0;
+        int totalWage = 0;
+        int workingHrs = 0;
+        int totalWorkedHrs = 0;
+
+        //Wage calculation
+        while (dayOfMonth <= working_days_per_month && totalWorkedHrs <= max_working_hrs)
+        {
+            checkPresence = (int) (Math.random()*10)%2; //Presence check
+            checkEmpType = (int) (Math.random()*10)%2;  //Work Type check
+            dayOfMonth++;
+            switch (checkPresence)
+            {
+                case IS_PRESENT:
+                    switch (checkEmpType)
+                    {
+                        case IS_FULL_TIME:
+                            workingHrs = 8;
+                            totalWorkedHrs += workingHrs;
+                            break;
+                        default:
+                            workingHrs = 4;
+                            totalWorkedHrs += workingHrs;
+                    }
+                    break;
+                default:
+                    totalWorkedHrs += 0;
+            }
+            totalWage = (wage_per_hr * totalWorkedHrs);
+        }
+        return totalWage;
+    }
+}
+
+//MAIN CLASS
+public class EmployeeWage
+{
+    public static void main(String[] args)
+    {
+        WageCalculation wc1 = new WageCalculation("company1", 20, 20, 100);
+        WageCalculation wc2 = new WageCalculation("company2", 10, 30, 110);
+        WageCalculation wc3 = new WageCalculation("company3", 15, 10, 120);
+        WageCalculation wc4 = new WageCalculation("company4", 30, 25, 130);
+        WageCalculation wc5 = new WageCalculation("company5", 50, 15, 140);
+
+        //calling instance methods and storing wages of employees in the array
+        int[] employeeWages = new int[5]; //declaring array
+
+        employeeWages[0] = wc1.wageCalculator();
+        employeeWages[1] = wc2.wageCalculator();
+        employeeWages[2] = wc3.wageCalculator();
+        employeeWages[3] = wc4.wageCalculator();
+        employeeWages[4] = wc5.wageCalculator();
+
+        for (int i=0;i<employeeWages.length;i++)
+        {
+            System.out.println("Employee monthly wage of company_"+(i+1)+": "+employeeWages[i]);
+        }
+    }
+}
+
+
